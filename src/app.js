a66ea52ebddc862cb6124adda554a82ef24342b4
Taeil Kwak <taeil2@gmail.com>
2020-06-05 13:34:39 -0500

    added bearer token

--- src/app.js
+++ src/app.js
@@ -17,6 +17,15 @@ app.use(helmet())
 app.use(cors())
 app.use(express.json());
 
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next()
+})
+
 app.get('/', (req, res) => {
     res.send('Hello, boilerplate!')
 })
